a9e8027690f426392632dfcdd71b0d962420a2a9
ThanhCong <nguyenthanhcong200895@gmail.com>
2018-06-24 09:56:34 +0700

    update

---
+++ QuanLySanPham/FormControllers/Login.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace QuanLySanPham
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.edt_username = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.edt_username);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(27, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(325, 191);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // edt_username
+            // 
+            this.edt_username.AccessibleName = "";
+            this.edt_username.Location = new System.Drawing.Point(75, 54);
+            this.edt_username.MaxLength = 30;
+            this.edt_username.Name = "edt_username";
+            this.edt_username.Size = new System.Drawing.Size(217, 20);
+            this.edt_username.TabIndex = 1;
+            this.edt_username.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.edt_username.TextChanged += new System.EventHandler(this.edt_username_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(14, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "                \r\n                \r\n                \r\n";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 280);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Login";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox edt_username;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ QuanLySanPham/FormControllers/Login.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLySanPham
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        
+        private void edt_username_TextChanged(object sender, EventArgs e)
+        {
+            if (edt_username.Text == "")
+            {
+
+            }
+        }
+    }
+}
