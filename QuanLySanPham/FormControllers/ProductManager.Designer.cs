0a1e9bcb987dee0485e23e5893a9b26569f444e5
ThanhCong <nguyenthanhcong200895@gmail.com>
2018-06-25 16:17:02 +0700

    update

--- QuanLySanPham/FormControllers/ProductManager.Designer.cs
+++ QuanLySanPham/FormControllers/ProductManager.Designer.cs
@@ -28,47 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btn_themsp = new System.Windows.Forms.Button();
+            this.btn_suasp = new System.Windows.Forms.Button();
+            this.btn_xoasp = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_tenuser = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btn_themuser = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_thoat = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // btn_themsp
             // 
-            this.button1.Location = new System.Drawing.Point(2, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Thêm sản phẩm";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_themsp.Location = new System.Drawing.Point(2, 12);
+            this.btn_themsp.Name = "btn_themsp";
+            this.btn_themsp.Size = new System.Drawing.Size(94, 23);
+            this.btn_themsp.TabIndex = 0;
+            this.btn_themsp.Text = "Thêm sản phẩm";
+            this.btn_themsp.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btn_suasp
             // 
-            this.button2.Location = new System.Drawing.Point(102, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Sửa sản phẩm";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btn_suasp.Location = new System.Drawing.Point(102, 12);
+            this.btn_suasp.Name = "btn_suasp";
+            this.btn_suasp.Size = new System.Drawing.Size(94, 23);
+            this.btn_suasp.TabIndex = 1;
+            this.btn_suasp.Text = "Sửa sản phẩm";
+            this.btn_suasp.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btn_xoasp
             // 
-            this.button3.Location = new System.Drawing.Point(202, 12);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(94, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Xóa sản phẩm";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btn_xoasp.Location = new System.Drawing.Point(202, 12);
+            this.btn_xoasp.Name = "btn_xoasp";
+            this.btn_xoasp.Size = new System.Drawing.Size(94, 23);
+            this.btn_xoasp.TabIndex = 2;
+            this.btn_xoasp.Text = "Xóa sản phẩm";
+            this.btn_xoasp.UseVisualStyleBackColor = true;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txt_tenuser);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(462, 12);
             this.groupBox1.Name = "groupBox1";
@@ -76,6 +77,16 @@
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Thông tin tài khoản";
+            // 
+            // txt_tenuser
+            // 
+            this.txt_tenuser.AutoSize = true;
+            this.txt_tenuser.Location = new System.Drawing.Point(98, 35);
+            this.txt_tenuser.Name = "txt_tenuser";
+            this.txt_tenuser.Size = new System.Drawing.Size(119, 13);
+            this.txt_tenuser.TabIndex = 1;
+            this.txt_tenuser.Text = "Tên người dùng hiển thị";
+            this.txt_tenuser.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label1
             // 
@@ -86,25 +97,15 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Tên người dùng:";
             // 
-            // label2
+            // btn_themuser
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(98, 35);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(119, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Tên người dùng hiển thị";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // button4
-            // 
-            this.button4.Enabled = false;
-            this.button4.Location = new System.Drawing.Point(302, 12);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(102, 23);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "Thêm người dùng";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btn_themuser.Enabled = false;
+            this.btn_themuser.Location = new System.Drawing.Point(302, 12);
+            this.btn_themuser.Name = "btn_themuser";
+            this.btn_themuser.Size = new System.Drawing.Size(102, 23);
+            this.btn_themuser.TabIndex = 4;
+            this.btn_themuser.Text = "Thêm người dùng";
+            this.btn_themuser.UseVisualStyleBackColor = true;
             // 
             // groupBox2
             // 
@@ -113,19 +114,29 @@
             this.groupBox2.Size = new System.Drawing.Size(688, 277);
             this.groupBox2.TabIndex = 5;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "List các sản phẩm được hiển thị trong đây";
+            this.groupBox2.Text = "Danh sách các sản phẩm";
+            // 
+            // btn_thoat
+            // 
+            this.btn_thoat.Location = new System.Drawing.Point(165, 56);
+            this.btn_thoat.Name = "btn_thoat";
+            this.btn_thoat.Size = new System.Drawing.Size(75, 23);
+            this.btn_thoat.TabIndex = 6;
+            this.btn_thoat.Text = "Thoát";
+            this.btn_thoat.UseVisualStyleBackColor = true;
             // 
             // ProductManager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(686, 387);
+            this.Controls.Add(this.btn_thoat);
             this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btn_themuser);
             this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_xoasp);
+            this.Controls.Add(this.btn_suasp);
+            this.Controls.Add(this.btn_themsp);
             this.Name = "ProductManager";
             this.Text = "ProductManager";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ProductManager_FormClosing);
@@ -139,13 +150,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btn_themsp;
+        private System.Windows.Forms.Button btn_suasp;
+        private System.Windows.Forms.Button btn_xoasp;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label txt_tenuser;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btn_themuser;
         private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_thoat;
     }
 }
